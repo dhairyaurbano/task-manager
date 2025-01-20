69ac5797ab24d73ef849f6410b5ea858b8567c6b
Dhairya Pandya <dhairyapandya05@gmial.com>
2025-01-20 09:49:08 +0530

    initial commit

---
+++ src/routes/Components/AppBar.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let title = "App Title";
+    export let subtitle = "App Subtitle";
+  </script>
+  
+  <div class="flex items-center w-full bg-white shadow p-4 border-b border-gray-300 z-10">
+    <img src="/back.png" alt="Back Icon" class="w-6 h-6 mr-4" />
+    <div>
+      <p class="text-black text-lg font-bold">{title}</p>
+      <p class="text-gray-500 text-sm">{subtitle}</p>
+    </div>
+  </div>
